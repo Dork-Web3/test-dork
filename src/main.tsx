238cca544402e343b124188148b726b4a2aed249
Dork-Web <dorkweb3@gmail.com>
2025-07-22 16:06:06 +0200

    Wyświetlanie initData w h1

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { InitData } from '@telegram-apps/sdk';
 import './index.css'
 import App from './App.tsx'
 
